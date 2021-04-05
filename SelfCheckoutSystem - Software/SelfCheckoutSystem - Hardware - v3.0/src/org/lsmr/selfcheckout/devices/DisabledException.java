9e656f2fe9c3c245c7960a1f827ffd84e5923d23
matthewJarrams <matthew.jarrams@gmail.com>
2021-04-05 14:25:27 -0600

    Fixed an issue with the last commit
    The project folder "software" contained all of the projects

--- SelfCheckoutSystem - Software/SelfCheckoutSystem - Hardware - v3.0/src/org/lsmr/selfcheckout/devices/DisabledException.java
+++
@@ -1,8 +0,0 @@
-package org.lsmr.selfcheckout.devices;
-
-/**
- * Issued when a device cannot perform its usual functions because it has been
- * disabled.
- */
-@SuppressWarnings("serial")
-public class DisabledException extends Exception {}
--- SelfCheckoutSystem - Software/SelfCheckoutSystem - Hardware - v3.0/src/org/lsmr/selfcheckout/devices/listeners/TouchScreenListener.java
+++
@@ -1,9 +0,0 @@
-package org.lsmr.selfcheckout.devices.listeners;
-
-import org.lsmr.selfcheckout.devices.AbstractDevice;
-
-/**
- * Represents listeners that respond only to enable/disable events on touch screens.
- */
-public interface TouchScreenListener extends AbstractDeviceListener {
-}
