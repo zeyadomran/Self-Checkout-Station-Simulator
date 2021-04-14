e4cda3cd1343dfcf98a20b12f5fbba3ccd8e0f1f
kirkelumir <56174765+kirkelumir@users.noreply.github.com>
2021-04-14 16:54:23 -0600

    Added tests for removePurchasedItems() method.

--- SelfCheckoutSystem - Software - Test/src/org/lsmr/selfcheckout/software/test/TestSuiteMain.java
+++ SelfCheckoutSystem - Software - Test/src/org/lsmr/selfcheckout/software/test/TestSuiteMain.java
@@ -26,6 +26,6 @@ import org.junit.runners.Suite;
 	ShutDownStartUpTest.class,
 	AttendantLooksUpProductTest.class,
 	CheckWeightTest.class,
-	GUITest.class
+	RemovePurchasedItemsTest.class
 })
 public class TestSuiteMain {}
